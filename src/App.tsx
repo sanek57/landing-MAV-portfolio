9b3c265c9f37bb8ae4c400ae446b7adec23be89d
sanek57 <sanekofblack@gmail.com>
2025-10-27 16:24:18 +1000

    fix: добавлена секция Skills

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import { About } from './components/About'
 import { Header } from './components/Header'
 import { Hero } from './components/Hero'
+import { Skill } from './components/Skill'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <main>
         <Hero />
         <About />
+        <Skill />
       </main>
     </>
   )
---
+++ src/components/Skill.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { skillItem } from '../constants'
+import { SkillCard } from './SkillCard'
+
+export const Skill = () => {
+  return (
+    <section className='section'>
+      <div className='container'>
+        <h2 className='headline-2'>Essential Tools I use</h2>
+
+        <p className='text-zinc-400 mt-3 mb-8 max-w-[50ch]'>
+          Discover the powerful tools and technologies I use to create
+          exceptional, high-performing websites & applications.
+        </p>
+
+        {/* auto-fill - растягивает до полной упаковки */}
+        <div className='grid gap-3 grid-cols-[repeat(auto-fill,_minmax(250px,_1fr))]'>
+          {skillItem.map(item => (
+            <SkillCard
+              key={item.label}
+              label={item.label}
+              imgSrc={item.imgSrc}
+              desc={item.desc}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  )
+}
